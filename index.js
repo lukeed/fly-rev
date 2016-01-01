9c58fa12b3fedcce2bfaf2ca6c5a02112f853b8c
lukee <luke.edwards05@gmail.com>
2016-01-01 15:36:08 -0800

    add modify filename pkg

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
-export default function (/*debug*/) {
-  this.filter("rev", (data, options) => {
-  /**
-    @overview A filter plugin returns an object { code, map, ext }
-    which is the result of transforming the incomding data source:
+const read = require("fs").readFileSync
+const assign = require("object-assign")
+const revHash = require("rev-hash")
+const revPath = require("rev-path")
+const modName = require("modify-filename")
+// const sortKeys = require("sort-keys")
 
-      return { code, map, ext }
+function generateName (filename) {
+  const buffer = read(filename)
+  const hash = revHash(buffer)
+  console.log( "inside gen", typeof file )
+  return file
+  // return modName(file, (name, extn) => {
+  //   console.log( "inside modName:", name, extn )
+  //   const idx = name.indexOf(".")
 
-    @example Sync filter `j` that transforms a given string into an
-    object, i.e, {code, map} where code is the result data and map
-    a sourcemap if `options.sourceMap === true`.
+  //   name = idx === -1 ? revPath(name, hash) : revPath(name.slice(0, idx), hash) + name.slice(idx)
 
-      const j = require("my-js-transformer")
-      const assign = require("object-assign")
+  //   return name + extn
+  // })
+}
 
-      export default function () {
-        return this.filter("j", (data, options) => {
-          return assign({ ext: ".js"}, j.render(data.toString(), options))
-        })
-      }
+export default function () {
+  let manifest = {}
+  this.rev = function (options) {
+    this.unwrap(files => files.map(name => {
+      const buff = read(name)
+      const hash = revHash(buff)
+      const idx = name.indexOf(".")
+      const extn = name.slice(idx)
 
-    @example Async filter `s` that transforms a given string and invokes
-    a callback function with an object, i.e, {css, map}.
+      let revved = (idx === -1) ? name : name.slice(0, idx)
+      revved = revPath(revved, hash) + (idx === -1 ? '' : extn)
 
-    const s = require("my-style-trasformer")
-    const assign = require("object-assign")
+      manifest[name] = revved
+    })).then(() => {
+      console.log( manifest );
+    })
 
-    export default function () {
-      return this.defer(s.render)(data.toString(), options).then((result) =>
-        assign({ ext: ".css"}, result))
-    }
-  */
-  })
+    // console.log( 'OUTISDE', manifest );
+
+      // console.log( filename, idx )
+      // console.log( filename )
+      // console.log( hash, file )
+    return this
+  }
+}
+
+function manifest(pth, options) {
+  if (typeof pth === 'string') {
+    pth = {path: pth};
+  }
+
+  const opts = assign({
+    path: 'rev-manifest.json',
+    merge: false
+  }, options)
+
+  let firstFileBase = null;
+  let manifest = {};
+
+  console.log( 'inside manifest', opts )
 }
